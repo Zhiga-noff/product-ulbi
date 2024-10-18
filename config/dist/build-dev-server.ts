9129849f7f698abc92bd579bef6491e3280bb1b7
kirill <kzhiganov@zvezdamedia.ru>
2024-10-18 22:56:55 +0300

    Так же подключил стили и добавил правила обработки scss

--- config/dist/build-dev-server.ts
+++ config/dist/build-dev-server.ts
@@ -1,14 +1,12 @@
 import {BuildOptions} from "./types/config";
 import type {Configuration as DevServerConfiguration} from "webpack-dev-server";
 
-export const buildDevServer = (options:BuildOptions):DevServerConfiguration => {
-  const ff = {
-    port: options.port,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  }
+export const buildDevServer = (options: BuildOptions): DevServerConfiguration => {
 
-
-  return ff
+    return {
+        port: options.port,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    }
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,19 @@
 import path from "path";
-import  webpack from 'webpack';
+import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/dist";
 import {BuildEnv, BuildPath} from "./config/dist/types/config";
 
 
-
-export default (env:BuildEnv)=>{
+export default (env: BuildEnv) => {
     const paths: BuildPath = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'dist'),
         html: path.resolve(__dirname, 'public', 'index.html')
     }
 
     const mode = env.mode || 'development'
     const isDev = mode === 'development'
-    const PORT =env.port || 3000
+    const PORT = env.port || 3000
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,

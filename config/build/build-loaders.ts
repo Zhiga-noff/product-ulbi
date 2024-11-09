70db755765f9c743f20e14f9ced594e3f7eed454
kirill <zhiganov_k.n@mail.ru>
2024-11-09 18:09:06 +0300

    Добавили бейбл и плагин обновления сайта без его перезагрузки в при дев режиме

--- config/build/build-loaders.ts
+++ config/build/build-loaders.ts
@@ -1,53 +1,67 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
-export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
-
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff|woff2|ttf|utf)$/i,
-        use: [
+export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => {
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
             {
-                loader: 'file-loader',
-                options: {name: '[name].[ext]', outputPath: 'images/'},
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
             },
+          ],
         ],
-    }
+      },
+    },
+  };
 
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    }
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]'
-                    },
-                }
-            },
-            "sass-loader",
-        ],
-    }
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2|ttf|utf)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+        options: { name: '[name].[ext]', outputPath: 'images/' },
+      },
+    ],
+  };
 
-    // Если не используем тайпскрипт - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash:base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
 
-    return [
-        fileLoader,
-        svgLoader,
-        typescriptLoader,
-        cssLoader,
-    ]
+  // Если не используем тайпскрипт - нужен babel-loader
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 };
--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,18 +1,21 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-export const buildPlugins = ({paths}: BuildOptions): webpack.WebpackPluginInstance[] => [
-    new HtmlWebpackPlugin({
-        template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(true)
-    })
+export const buildPlugins = ({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] => [
+  new HtmlWebpackPlugin({
+    template: paths.html,
+  }),
+  new webpack.ProgressPlugin(),
+  new MiniCssExtractPlugin({
+    filename: 'css/[name].[contenthash:8].css',
+    chunkFilename: 'css/[name].[contenthash:8].css',
+  }),
+  new webpack.DefinePlugin({
+    __IS_DEV__: JSON.stringify(true),
+  }),
+  new webpack.HotModuleReplacementPlugin(),
 ];

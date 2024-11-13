f900cc5b22a64e1862f757d8d2cb9d9554d6e4a7
kirill <kzhiganov@zvezdamedia.ru>
2024-11-13 17:21:34 +0300

    Сделали первый тест

--- src/shared/lib/class-names/class-names.test.ts
+++ src/shared/lib/class-names/class-names.test.ts
@@ -1,7 +1,37 @@
 import {classNames} from "./class-names";
 
 describe('Функция classNames', function () {
-    test('test', () => {
-        expect(classNames('string')).toBe('string')
+    test('С одним параметром', () => {
+        expect(classNames('someClass')).toBe('someClass')
+    })
+
+    test('с первым параметром класса и массивом классов', () => {
+        const expected = 'someClass class1 class2'
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected)
+    })
+
+    test('со всеми параметрами в функции', () => {
+        const expected = 'someClass class1 class2 hovered scrollable'
+        expect(classNames('someClass',
+            {hovered: true, scrollable: true},
+            ['class1', 'class2']))
+            .toBe(expected)
+    })
+
+    test('один из переданных модов false', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames('someClass',
+            {hovered: true, scrollable: false},
+            ['class1', 'class2']))
+            .toBe(expected)
+    })
+
+    test('один из переданных модов false', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames('someClass',
+            {hovered: true, scrollable: undefined},
+            ['class1', 'class2']))
+            .toBe(expected)
     })
 });
